332a6c6780701e1c15dd26c1b100b275c7866c15
Denis Solovyov <solovyov.denis@gmail.com>
2017-11-02 23:13:21 +0200

    Finish task1 - square root

---
+++ sqrt/index.php
@@ -0,0 +1,22 @@
+<?php
+class MySqrt
+{
+	public function skrt($value,$precision)
+	{
+		if ($value > 0){
+			$x = 1;
+			while (abs($x * $x - $value) > 0.00001){
+				$x = 0.5 * ($x + $value/$x);
+			}
+			return round($x,$precision);
+		}elseif($value == 0){
+			return 0;
+		}else{
+			return "Number cannot be less zero";
+		}
+	}
+}
+
+$a = new MySqrt;
+echo $a->skrt(5,3);
+?>
