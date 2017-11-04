31103e51d9e4feb7f6cde1b7427bab10b2284928
Denis Solovyov <solovyov.denis@gmail.com>
2017-11-05 01:26:56 +0200

    Changed on less count iterationss

--- friday13/index.php
+++ friday13/index.php
@@ -12,9 +12,9 @@ function countFriday($year_from, $year_to)
 	$count = 0;
 	for ($i=$year_from;$i<=$year_to;$i++){
 		if (checkLeapYear($i)){
-			$first_jan = $i."-01-01";
-			for ($j=1;$j<366;$j++){
-				$temp_date = strtotime($first_jan ."+". $j . "day");
+			$thriteen_jan = $i."-01-13";
+			for ($j=1;$j<=12;$j++){
+				$temp_date = strtotime($thriteen_jan ."+". $j . "month");
 				if (getdate($temp_date)['mday'] == 13 && getdate($temp_date)['weekday'] == 'Friday'){
 					$count++;
 					// echo "<pre>".print_r(getdate($temp_date),true)."</pre><br />";
